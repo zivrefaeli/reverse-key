2a40d64f7efc60bcd5d1897d05fad3b0a161fd7a
refaeli <refaeliziv@gmail.com>
2024-06-11 13:20:19 +0300

    Create reverse_key filter plugin

---
+++ fluentd/plugins/filter_reverse_key.rb
@@ -0,0 +1,25 @@
+require 'fluent/plugin/filter'
+
+module Fluent::Plugin
+  class ReverseKey < Filter
+    Fluent::Plugin.register_filter('reverse_key', self)
+
+    desc 'The key to reverse'
+    config_param :key, :string, default: 'message'
+
+    def configure(conf)
+      super
+    end
+
+    def filter(tag, time, record)
+      begin
+        record[@key] = record[@key].reverse
+      rescue
+        log.error('failed to reverse %s at %s' % [@key, record])
+        return nil
+      else
+        return record
+      end
+    end
+  end
+end
